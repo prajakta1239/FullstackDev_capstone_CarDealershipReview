87db5d19150991a58b629d35426f2e50e74bc7a5
prajakta1239 <prajakta.k137@gmail.com>
2025-08-25 09:43:31 -0400

    modified files for deaker views

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,8 +16,14 @@ urlpatterns = [
     path("logout/", views.logout_request, name="logout"),
 
     # path for dealer reviews view
-
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
     # path for add a review view
+    # path for get dealership all
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    
+    # path for get dealer details a/c ID
+        path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
 
     #path for get cars get_cars
     path(route='get_cars', view=views.get_cars, name ='getcars'),
