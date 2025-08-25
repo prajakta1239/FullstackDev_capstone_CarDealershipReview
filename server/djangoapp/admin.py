5144b7867475027f9c6a5061434c770c08e93991
prajakta1239 <prajakta.k137@gmail.com>
2025-08-25 07:48:00 -0400

    Build CarModel and CarMake Django Models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,28 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+    extra = 2
+
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'type', 'year', 'dealer_id')
+    list_filter = ('car_make', 'type', 'year')
+    search_fields = ('name', 'car_make__name')
+
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    inlines = [CarModelInline]
+
 
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,25 +1,44 @@
-# Uncomment the following imports before adding the Model code
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+# Car Make model
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
 
-
-# Create your models here.
-
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+    def __str__(self):
+        return self.name
 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+# Car Model model
+class CarModel(models.Model):
+    # Many-to-One relationship with CarMake
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE, related_name="models")
+    
+    # Dealer Id referring to dealer in Cloudant DB
+    dealer_id = models.IntegerField()
+    
+
+    name = models.CharField(max_length=100)
+
+    # Limited choices for type
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        ('TRUCK', 'Truck'),
+        ('COUPE', 'Coupe'),
+    ]
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
+
+    # Year as DateField (only storing year)
+    year = models.DateField()
+   
+   
+
+    # Optional field
+    color = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.car_make.name} {self.name} ({self.year.year})"
